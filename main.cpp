2e63606d9cab31c5014bc921ef0fd8d1431e8e3a
JoelA <joeladu23@gmail.com>
2025-05-21 18:13:26 -0400

    pointer

--- main.cpp
+++ main.cpp
@@ -2,3 +2,7 @@
 #include <allegro5/allegro_primitives.h>
 
 // added allegro primitives header 
+
+float x = 400, y = 300; // pointer position (center screen)
+float speed = 5.0;      // how fast it moves
+
